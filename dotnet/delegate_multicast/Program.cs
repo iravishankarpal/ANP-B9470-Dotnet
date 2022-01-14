b1eecde89d49ce1f11d159a8823c2d9be47f6b95
iravishankarpal <iravishankarpal@gmail.com>
2022-01-14 19:06:42 +0530

    deleget multicast

--- dotnet/delegate_multicast/Program.cs
+++ dotnet/delegate_multicast/Program.cs
@@ -4,9 +4,34 @@ namespace delegate_multicast // Note: actual namespace depends on the project na
 {
     internal class Program
     {
+        public delegate void delmethod(int x, int y);
+        public class TestMultipleDelegate
+            {
+            public void plus_Method1(int x, int y)
+                {
+                Console.Write("You are in plus_Method");
+                Console.WriteLine(x + y);
+                }
+
+            public void subtract_Method2(int x, int y)
+                {
+                Console.Write("You are in subtract_Method");
+                Console.WriteLine(x - y);
+                }
+            }
         static void Main(string[] args)
         {
-           
+         TestMultipleDelegate obj = new TestMultipleDelegate();
+            delmethod del = new delmethod(obj.plus_Method1);
+            del += new delmethod(obj.subtract_Method2);
+            // plus_Method1 and subtract_Method2 are called
+            del(50, 10);
+            Console.WriteLine();
+            //Here again we have multicast
+            del -= new delmethod(obj.plus_Method1);
+            //Only subtract_Method2 is called
+            del(20, 10);
+            Console.ReadLine();
         }
     }
 }
