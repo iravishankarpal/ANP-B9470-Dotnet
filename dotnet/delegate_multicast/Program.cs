0d8213e46de93f88190e03168f50114ceb7fcb11
iravishankarpal <iravishankarpal@gmail.com>
2022-01-14 18:57:14 +0530

    add multicast

---
+++ dotnet/delegate_multicast/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace delegate_multicast // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
