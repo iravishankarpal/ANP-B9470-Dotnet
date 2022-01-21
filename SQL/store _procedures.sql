2beb726c91805c6fc70abc1fb3bcd8b2a1ad4cac
iravishankarpal <iravishankarpal@gmail.com>
2022-01-21 11:22:10 +0530

    prameter store proc

--- SQL/store _procedures.sql
+++ SQL/store _procedures.sql
@@ -1,5 +1,8 @@
+select * from employee
 
-create proc proc_employee
+
+-- its as function in sql 
+create proc proc_employ
 as 
 begin
 	select 
@@ -12,17 +15,48 @@ begin
 		employee_name asc;
 end;
 
-
-exec proc_employee;
-
--- employee_name        employee_city        employee_phoneno
--- -------------------- -------------------- ----------------
--- aman                 west                 88888888
--- dhamu                south                88888888
--- ravi                 ambernat             88888888
--- tooo                 esat                 333333333
+--ececutioni command
+proc_employ
+exec proc_employ
 
 
+employee_name        employee_city        employee_phoneno
+-------------------- -------------------- ----------------
+aman                 west                 88888888
+dhamu                south                88888888
+ravi                 ambernat             88888888
+tooo                 esat                 333333333
+
+
+-- store procedures funcion with parameters 
+
+change and create are  two key word are use to create  this function
+is  and as are key word for sepation from decelaration and main function
+
+
+proc_employ
+
+alter proc proc_employ
+@employee_phoneno as int
+as 
+begin
+
+	update employee
+	set employee_phoneno = @employee_phoneno
+	where employee_name = 'ravi'
+end
+
+proc_employ
+proc_employ 123
+
+
+
+
+
+
+
+
+--alter in store procedures may lead to crash the sp fuction
 
 alter proc proc_employee
 as
@@ -35,20 +69,8 @@ end;
 alter proc proc_employee
 as
 begin 
-	select * from proc_employee;
+	select * from proc_employee
+
 end;
 
 
-
-select * from proc_employee;
-
-
-select proc_employee
-as 
-begin
-	select 
-		*
-	from 
-		proc_employee
-	
-end;
