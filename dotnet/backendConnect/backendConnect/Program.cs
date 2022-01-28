0e1a35ab8d1c06ee87a5c5ac8987a0a3853e42b5
iravishankarpal <iravishankarpal@gmail.com>
2022-01-28 10:27:35 +0530

    added dot net connection to backend

---
+++ dotnet/backendConnect/backendConnect/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace backendConnect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SqlConnection con = new SqlConnection(@"Data Source=.;Initial Catalog=ravi;Integrated Security=True");
+            SqlDataReader rdr = null;
+            try
+            {
+                con.Open();
+
+                Console.WriteLine("Connection Open");
+                SqlCommand cmd = new SqlCommand("select employee_name from employee", con);
+
+                rdr = cmd.ExecuteReader();
+
+                // print the name of each record
+                while (rdr.Read())
+                {
+                    Console.WriteLine(rdr[0]);
+                }
+                con.Close();
+            }
+            catch
+            {
+
+                Console.WriteLine("Connection Cant open");
+
+
+            }
+           
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dotnet/backendConnect/backendConnect/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("backendConnect")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("backendConnect")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("04497fc4-ca0d-43f4-8bda-c0bc5c6b3963")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
