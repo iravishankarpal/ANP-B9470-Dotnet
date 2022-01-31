fa920e3298ebd2d165b994e5b6667e7698ca99b6
iravishankarpal <iravishankarpal@gmail.com>
2022-01-31 18:17:23 +0530

    husse

---
+++ dotnet/consoleapp/consoleapp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace consoleapp
+{
+    class Program
+    {
+        static void greating(string fname = "students")
+        {
+            Console.WriteLine($"hello {fname}!!");
+        }
+
+        static void Main(string[] args)
+        {
+            greating("ravi");
+            greating("anuj");
+            greating("achal");
+            greating();
+        }
+
+    }
+
+}
