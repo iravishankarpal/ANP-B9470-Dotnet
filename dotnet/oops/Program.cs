c15fd7f99da5ed0c4bc241bc9cea3f046c32e7e5
iravishankarpal <iravishankarpal@gmail.com>
2022-02-02 15:52:10 +0530

    added wpf

---
+++ dotnet/oops/Program.cs
@@ -0,0 +1,19 @@
+﻿// oops gose here
+using System;
+
+namespace oops{
+    class Program{
+        static void greating(string fname="student"){
+            Console.WriteLine($"hi {fname}");
+        }
+        
+        static void Main(){
+             //fundtions in c# 
+            greating("ram");
+            greating("ravi");
+            greating();
+
+
+        }
+    }
+}
