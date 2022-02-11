02b2241bb28373d68f3f66b5625308bf804785d9
iravishankarpal <iravishankarpal@gmail.com>
2022-02-11 12:34:20 +0530

    added

---
+++ dotnet/LINQ/WebForm1.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQ_Project
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PradeepDataContext db = new PradeepDataContext();
+            if (!Page.IsPostBack)
+            {
+                GridView1.DataSource = from ed in db.EmployeeDetails
+                                       select new
+                                       {
+                                           EmployeeName = ed.EmpName,
+                                           Location = ed.Location,
+                                           Gender = ed.Gender
+                                       };
+                GridView1.DataBind();
+
+            }
+        }
+    }
+}
